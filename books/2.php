233daa31fd722051afd7b466d96d90aec4b10af3
armeniasadikaj <armeniasadikaj@gmail.com>
2024-04-21 22:23:19 +0200

    modified:   books/1.php 	modified:   books/2.php 	modified:   books/3.php 	modified:   books/4.php 	modified:   books/books1.php

--- books/2.php
+++ books/2.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="../header/header.css">
     <link rel="stylesheet" href="../footer/footer.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://unpkg.com/htmlincludejs"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style>
