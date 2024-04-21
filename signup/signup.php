dde9b2f17e8bed4efcc66e118c0b3c2b121bac23
Besarta Mustafa <besartamustafa49@gmail.com>
2024-04-21 15:09:54 +0200

    modified:   signup/signup.php

--- signup/signup.php
+++ signup/signup.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (!empty($username) && !empty($password)) {
             // Assuming successful login, redirecting to home2.html
             $_SESSION['username'] = $username;
-            header("Location: ../home html/home2.html");
+            header("Location: ../home html/home2.php");
             exit();
         } else {
             echo "Please fill in all required fields.";
