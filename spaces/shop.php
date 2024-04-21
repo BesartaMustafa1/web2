b2708cc5642de4b6cac17378f8e97138038b16b3
Besarta Mustafa <besartamustafa49@gmail.com>
2024-04-21 12:25:41 +0200

    modified:   spaces/shop.php

--- spaces/shop.php
+++ spaces/shop.php
@@ -81,7 +81,11 @@ if (isset($_POST['confirm_order'])) {
 <head>
     <meta charset="UTF-8">
     <title>Cart</title>
-    <!-- Add your CSS and JS links here -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../header/header.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 </head>
 <style>
     /* Basic table styling */
@@ -214,11 +218,13 @@ a:hover {
     <a href="shop.php?sort_by=name">Sort by Name</a> | 
 <a href="shop.php?sort_by=price">Sort by Price</a>
 
-<script>
+<script>        
     $(function () {
-            $('#header').load('../header/header.html');
-        });
+            $('#header').load('../header/header.html');       
+        }); 
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
    
 </body>
 </html>
