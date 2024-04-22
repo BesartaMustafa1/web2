4e0cb2dc7d2185f232d531355bf1a43741a70291
ElisaBerisha <elisaberisha888@gmail.com>
2024-04-21 18:23:19 -0700

    changes

--- launching/launching.php
+++ launching/launching.php
@@ -21,7 +21,7 @@ function signOut() {
 ?>
 
 <script>
-    $('#header').load('../header/header.html');
+    $('#header').load('../header/header.php');
 </script>
 
 <?php
@@ -61,7 +61,7 @@ echo '</script>';
     <link rel="stylesheet" href="../header/header.css">
     <link rel="stylesheet" href="../footer/footer.css">
     <link rel="stylesheet" href="../courses/courses.css">
-    <link rel="stylesheet" href="/launching/launching.html">
+    <link rel="stylesheet" href="/launching/launching.php">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/htmlincludejs"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
