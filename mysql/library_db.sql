5a86714d2c98b93600273644aa65c4dcfbcb9cd7
BrinetaUka <brineta.uka@student.uni-pr.edu>
2024-05-27 13:14:10 +0200

    db connector

---
+++ mysql/library_db.sql
@@ -0,0 +1,29 @@
+Use library_db;
+
+-- Table for authors
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Table for books
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT,
+    published_date DATE,
+    isbn VARCHAR(13),
+    pages INT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Table for members
+CREATE TABLE members (
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(15),
+    join_date DATE
+);
+
+
