bb43dfeb63e585f3399c3af0df8b47da814957a7
ElisaBerisha <elisaberisha888@gmail.com>
2024-05-29 21:23:20 -0700

    changes

--- launching/launching.php
+++ launching/launching.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // Retrieve the username from session storage
 $username = isset($_SESSION["username"]) ? $_SESSION["username"] : null;
 
@@ -62,14 +63,19 @@ echo '</script>';
     <link rel="stylesheet" href="../footer/footer.css">
     <link rel="stylesheet" href="../courses/courses.css">
     <link rel="stylesheet" href="/launching/launching.php">
+    <link rel="stylesheet" href="/launching/get_user.php">
+    <link rel="stylesheet" href="/launching/update_user.php">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/htmlincludejs"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/htmlincludejs"></script>
+    <script src="script.js" defer></script>
+ 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
  </head>
     <body>
+
     <?php include '../header/header.php'; ?>
         <!-- <header 
             <nav>
@@ -140,6 +146,18 @@ echo '</script>';
 
 <img src="rocket.png" class="rocket">
 </div>
+
+
+ <!-- AJAX Section -->
+ <div class="ajax-section" style="display: block;">
+    <h2>AJAX User Data</h2>
+    <div id="result">User data will be loaded here.</div>
+    <button onclick="loadData()" style="display: block;">Load User</button>
+    <button onclick="updateUser()" style="display: block;">Update User</button>
+</div>
+
+
+
 
 <!-- 
         <script>
